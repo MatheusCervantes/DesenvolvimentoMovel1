5d28ace55e8c6bd0942eac8cb3484f95c43c59df
Matheus Cervantes <matheuscervantes1@gmail.com>
2024-11-27 10:18:25 -0300

    Add layouts Trabalho Final

---
+++ TrabalhoFinal/app/src/main/java/com/example/trabalhofinal/Principal.java
@@ -0,0 +1,13 @@
+package com.example.trabalhofinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Principal extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_sobre);
+    }
+}
---
+++ TrabalhoFinal/app/src/main/java/com/example/trabalhofinal/Simulacao.java
@@ -0,0 +1,12 @@
+package com.example.trabalhofinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Simulacao extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_principal);
+    }
+}
